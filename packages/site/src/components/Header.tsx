69b0c8db4e2629c7f6f56c9df7401a503d2db7a1
Niraj Kamdar <nirajkamdar27@pm.me>
2022-12-03 19:36:29 +0530

    feat: add header

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -63,7 +63,7 @@ export const Header = ({
     <HeaderWrapper>
       <LogoWrapper>
         <SnapLogo color={theme.colors.icon.default} size={36} />
-        <Title>Snappy Wraps</Title>
+        <Title>Polysnap</Title>
       </LogoWrapper>
       <RightContainer>
         <Toggle
